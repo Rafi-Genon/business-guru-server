628cef546e4492b544942d563dccbf293b003f4b
Rafi-Genon <mdabulhasannurrafi@gmail.com>
2021-04-16 11:37:49 +0600

    starting database

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+const cors = require('cors')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+const port =  5050
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+// businessGuru
+// serviceList
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y23oh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const serviceCollection = client.db("businessGuru").collection("serviceList");
+ 
+    
+});
+app.get('/',(req,res)=>{
+    res.send('workiniing')
+    console.log('it is /');
+})
+
+app.listen(port, () => {
+    console.log(`Example appa listening at http://localhost:${port}`)
+  })
