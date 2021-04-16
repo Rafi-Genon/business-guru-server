0fe2421e078cff52c10640fd4afb9d258b664db7
Rafi-Genon <mdabulhasannurrafi@gmail.com>
2021-04-16 22:57:18 +0600

    starting review page

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 
-const port =  5050
+const port = 5050
 
 const app = express()
 app.use(cors())
@@ -18,14 +18,29 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const serviceCollection = client.db("businessGuru").collection("serviceList");
- 
-    
+
+    app.post('/addService', (req, res) => {
+        const newServiceData = (req.body)
+        serviceCollection.insertOne(newServiceData)
+            .then(result => {
+                console.log(result);
+                res.send(result.insertedCount > 0)
+            })
+        // const newService = req
+    })
+
+    app.get('/services', (req, res) => {
+        serviceCollection.find()
+            .toArray((err, services) => {
+                res.send(services)
+            })
+    })
 });
-app.get('/',(req,res)=>{
-    res.send('workiniing')
-    console.log('it is /');
-})
+// app.get('/', (req, res) => {
+//     res.send('workiniing')
+//     console.log('it is /');
+// })
 
 app.listen(port, () => {
     console.log(`Example appa listening at http://localhost:${port}`)
-  })
+})
