c8c4788a5d9b746c035889533321c5fac9b7c06a
Rafi-Genon <mdabulhasannurrafi@gmail.com>
2021-04-17 20:07:27 +0600

    starting booking list

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const serviceCollection = client.db("businessGuru").collection("serviceList");
+    const reviewsCollection = client.db("businessGuru").collection("review");
+    const ordersCollection = client.db("businessGuru").collection("orders");
 
     app.post('/addService', (req, res) => {
         const newServiceData = (req.body)
@@ -26,7 +28,6 @@ client.connect(err => {
                 console.log(result);
                 res.send(result.insertedCount > 0)
             })
-        // const newService = req
     })
 
     app.get('/services', (req, res) => {
@@ -34,6 +35,52 @@ client.connect(err => {
             .toArray((err, services) => {
                 res.send(services)
             })
+    })
+
+    app.post('/addReview', (req, res) => {
+        const newReviewData = (req.body)
+        reviewsCollection.insertOne(newReviewData)
+            .then(result => {
+                console.log(result);
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/review', (req, res) => {
+        reviewsCollection.find()
+            .toArray((err, reviews) => {
+                res.send(reviews)
+            })
+    })
+
+    app.post('/addOrder', (req, res) => {
+        const newOrderData = (req.body)
+        ordersCollection.insertOne(newOrderData)
+            .then(result => {
+                console.log(result);
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/allOrders', (req, res) => {
+        ordersCollection.find()
+            .toArray((err, orders) => {
+                res.send(orders)
+            })
+    })
+
+    app.patch('/update/:id', (req, res) => {
+        console.log(req.body, req.params);
+        const id = req.params.id
+        ordersCollection.updateOne({ _id: ObjectID(id) },
+            {
+                $set: { status: req.body.status }
+            })
+            .then(result => {
+                console.log(result),
+                    res.send(modifiedCount > 0)
+            })
+
     })
 });
 // app.get('/', (req, res) => {
